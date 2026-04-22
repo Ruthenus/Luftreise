977a69073fb214eb870882e6406e38728956c200
Maxym <Maxym@DESKTOP-HS5N4TR>
2026-04-22 22:10:01 +0300

    Commit

--- Controllers/FlightsController.cs
+++ Controllers/FlightsController.cs
@@ -1,115 +1,89 @@
-<<<<<<< HEAD
 using Luftreise.Application.Interfaces;
-
 using Luftreise_Command_project_.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 
 namespace Luftreise_Command_project.Controllers
-{ 
-    public class FlightsController : Controller
-    {
-        private readonly IBookingRepository _bookingRepository;
-        private readonly IFlightRepository _flightRepository;
-    
-
-    
-    public FlightsController(IBookingRepository bookingRepository, IFlightRepository flightRepository)
-        {
-            _bookingRepository = bookingRepository;
-            _flightRepository = flightRepository;
-      
-    }
-=======
-﻿using Luftreise.Application.Interfaces;
-using Luftreise_Command_project_.Models;
-using Microsoft.AspNetCore.Mvc;
-
-namespace Luftreise_Command_project_.Controllers
 {
-    public class FlightsController : Controller
+  public class FlightsController : Controller
+  {
+    private readonly IBookingRepository _bookingRepository;
+    private readonly IFlightRepository _flightRepository;
+
+    public FlightsController(IBookingRepository bookingRepository, IFlightRepository flightRepository)
     {
-        private readonly IBookingRepository _bookingRepository;
-
-        public FlightsController(IBookingRepository bookingRepository)
-        {
-            _bookingRepository = bookingRepository;
-        }
->>>>>>> 595becd4d01a77026a22bab0118abd03b0a43f8b
-
-        [HttpGet]
-        public IActionResult Flights()
-        {
-            return View();
-        }
-
-        [HttpGet]
-        public IActionResult Booking(
-            int id,
-            string airline,
-            string flightNumber,
-            string fromCity,
-            string toCity,
-            string departureTime,
-            string arrivalTime,
-            DateTime flightDate,
-            string flightClass,
-            double price)
-        {
-            var userEmail = HttpContext.Session.GetString("UserEmail");
-
-            var model = new BookingViewModel
-            {
-                FlightId = id,
-                Airline = airline,
-                FlightNumber = flightNumber,
-                FromCity = fromCity,
-                ToCity = toCity,
-                DepartureTime = departureTime,
-                ArrivalTime = arrivalTime,
-                FlightDate = flightDate,
-                FlightClass = flightClass,
-                Email = userEmail,
-                Price = price
-            };
-
-            return View(model);
-        }
-
-        [HttpGet]
-        public IActionResult AirportPayment(BookingViewModel model)
-        {
-            return View(model);
-        }
-
-        [HttpPost]
-        public IActionResult Booking(BookingViewModel model)
-        {
-            if (!ModelState.IsValid)
-                return View(model);
-
-            var userEmail = HttpContext.Session.GetString("UserEmail");
-
-            if (string.IsNullOrEmpty(userEmail))
-                return RedirectToAction("Login", "Account");
-
-            return View("AirportPayment", model);
-        }
-<<<<<<< HEAD
-       
-        [HttpPost]
-        public async Task<IActionResult> Search(SearchModels model)
-        {
-              var flights = await _flightRepository.SearchFlightsAsync(model.From,
-              model.To,
-              model.FlightDate);
-            return View("Flights", flights);
-=======
-
-        [HttpPost]
-        public IActionResult Search(SearchModels model)
-        {
-            return View("Flights", model);
->>>>>>> 595becd4d01a77026a22bab0118abd03b0a43f8b
-        }
+      _bookingRepository = bookingRepository;
+      _flightRepository = flightRepository;
     }
+
+    [HttpGet]
+    public IActionResult Flights()
+    {
+      return View(new List<Luftreise.Domain.Entities.Flight>());
+    }
+
+    [HttpGet]
+    public IActionResult Booking(
+        int id,
+        string airline,
+        string flightNumber,
+        string fromCity,
+        string toCity,
+        string departureTime,
+        string arrivalTime,
+        DateTime flightDate,
+        string flightClass,
+        double price)
+    {
+      var userEmail = HttpContext.Session.GetString("UserEmail");
+
+      var model = new BookingViewModel
+      {
+        FlightId = id,
+        Airline = airline,
+        FlightNumber = flightNumber,
+        FromCity = fromCity,
+        ToCity = toCity,
+        DepartureTime = departureTime,
+        ArrivalTime = arrivalTime,
+        FlightDate = flightDate,
+        FlightClass = flightClass,
+        Email = userEmail,
+        Price = price
+      };
+
+      return View(model);
+    }
+
+    [HttpGet]
+    public IActionResult AirportPayment(BookingViewModel model)
+    {
+      return View(model);
+    }
+
+    [HttpPost]
+    public IActionResult Booking(BookingViewModel model)
+    {
+      if (!ModelState.IsValid)
+        return View(model);
+
+      var userEmail = HttpContext.Session.GetString("UserEmail");
+
+      if (string.IsNullOrEmpty(userEmail))
+        return RedirectToAction("Login", "Account");
+
+      return View("AirportPayment", model);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Search(SearchModels model)
+    {
+      var flights = await _flightRepository.SearchFlightsAsync(
+          model.From,
+          model.To,
+          model.FlightDate);
+
+      return View("Flights", flights);
+    }
+  }
 }
